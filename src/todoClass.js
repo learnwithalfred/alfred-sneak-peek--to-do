d06dd96e8456a242922954f09bcfbbf0089cd25e
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-01 12:12:08 +0000

    Remove unwanted files

--- src/todoClass.js
+++
@@ -1,7 +0,0 @@
-export default class Todo {
-  constructor(description) {
-    this.description = description;
-    this.completed = false;
-    this.index = Math.floor(Math.random() * 1000000);
-  }
-}
